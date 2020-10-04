c83aceb54d331781f4b563694ff2ce836b27cb0b
DaemonSharps <56834210+DaemonSharps@users.noreply.github.com>
2020-10-04 23:21:08 +0300

    add top and bottom of all pages

---
+++ blocks/footer.php
@@ -0,0 +1,16 @@
+<footer class="mastfoot mt-auto text-center" style="background-color:rgba(7, 7, 7,0.73) ;">
+	<hr class="featurette-divider" style=" width:100%; height:3px; background-color:#FFFFFF;  ">
+        <div class="inner">
+          <p class="text-white footer" style="margin-bottom:0px;padding:10px; display: flex; justify-content: space-around; ">#БУДЬ_СОБОЙ  
+          	<a href="https://tlgg.ru/GrizzliPizza" class ="text-reset text-white">  Telegram для связи</a>
+          	<a>Все права защищены ©</a> 
+          	<a href="https://passport.webmoney.ru/asp/certview.asp?wmid=356702467712" target=_blank><IMG SRC="https://www.webmoney.ru/img/icons/certificate/135_wm_merchant.png" title="Здесь находится аттестат нашего WM идентификатора " border="0"><br></a>
+          		 <a><IMG SRC="https://www.webmoney.ru/img/icons/88x31_wm_black.png" title="Мы используем WebMoney" border="0"><br>
+          		</a>
+          	</p>
+
+          
+          <!-- begin WebMoney Transfer : attestation label --> <!-- end WebMoney Transfer : attestation label -->
+         
+        </div>
+      </footer>
---
+++ blocks/head.php
@@ -0,0 +1,31 @@
+<link rel="icon" type="image/png" href="https://www.body-in-12-days.xyz/img/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="https://www.body-in-12-days.xyz/img/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="https://www.body-in-12-days.xyz/img/favicon-48x48.png" sizes="48x48">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(63273442, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/63273442" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+	<meta charset="utf-8">
+	<!-- <meta name="vievport" content="width=device-width, inital-scale=1, shrink-to-fit=no"> --> <!-- /// включить как сделаю адаптивную -->
+	<meta https-equiv="X-UA-Compatible" content="ie=edge">
+  
+ <meta name="keywords" content="Фитнес, спорт, здоровье, идеальное тело за 12 дней, идеалное тело" />
+ <meta name = "robots" content = "index,follow" />
+	<link rel="stylesheet" href="https://www.body-in-12-days.xyz/css/style.css">
+	<link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://www.body-in-12-days.xyz/css/try.css">
+  <link rel="stylesheet" href="https://www.body-in-12-days.xyz/css/tryMedia.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
