a1e917b80c64caba432123557afdfe523eb45b31
DaemonSharps <56834210+DaemonSharps@users.noreply.github.com>
2020-10-04 23:16:13 +0300

    Java script for slider on phone screen

---
+++ js/my-slick.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+$(".slider").slick({
+	arrows:false,
+	dots:false,
+	adaptiveHeight:false,
+	slidesToShow:1,
+	slidesToScroll:1,
+	speed:600,
+	easing:'linear',
+	infinite:false,
+	initialSlide:1,
+	autoplay:false,
+	draggable:true,
+	swipe:true,
+	touchTreshold:20,
+	touchMove:true,
+	waitForAnimate:false,
+	centerMode:true,
+	variableWidth:true,
+	centerPadding:'20px',
+	focusOnSelect:true,
+	
+
+
+
+
+});
+});
