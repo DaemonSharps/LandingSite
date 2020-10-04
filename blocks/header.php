d830095b21a8fda207e607b6c1c44dfa4eee7e6e
DaemonSharps <56834210+DaemonSharps@users.noreply.github.com>
2020-10-04 23:20:11 +0300

    some blocks for speed up page creation process

---
+++ blocks/header.php
@@ -0,0 +1,31 @@
+<nav class="site-header shadow mb-2 " >
+  <div style="background-color:rgba(7, 7, 7,0.73); " class="header-bord" id="header">
+     <div class="container d-flex bd-highlight" style="max-width: 1750px;  ">
+         <div class="p-2  bd-highlight">
+  	       <a class="navbar-brand mt-1 text-white text-decoration-none" style="font-size:25px ; font-weight:100;" href="/" >#БУДЬ_СОБОЙ</a>
+         </div>
+            <div class="p-2 d-flex justify-content-start flex-grow-1 bd-highlight">
+       <a style="font-size:25px ; font-weight:100;border-right: 1px solid #ffffff; padding-right: 5px; " class=" mt-2 d-none d-md-inline-block font-weight-bold  text-decoration-none buttoncolor" href="https://www.body-in-12-days.xyz/demo.php" target="_block">ДЕМО</a>
+  		 <a style="font-size:25px ; font-weight:100; margin:5px; " class="text-center mt-2 d-none d-md-inline-block font-weight-bold text-decoration-none buttoncolor" href="https://www.body-in-12-days.xyz/buy.php" target="_block">КУПИТЬ</a>
+
+  		
+
+            </div>
+        <div class="nav-toggle"><span id="burger"></span></div>
+        <ul id="menu" class="">
+        <li><a style="font-size:25px ; font-weight:100; padding-right: 5px; " class=" mt-2  d-md-inline-block font-weight-bold  text-decoration-none buttoncolor" href="https://www.body-in-12-days.xyz/demo.php" target="_block">ДЕМО</a></li>
+        <li><a style="font-size:25px ; font-weight:100; margin:5px; " class="text-center mt-2  d-md-inline-block font-weight-bold text-decoration-none buttoncolor" href="https://www.body-in-12-days.xyz/buy.php" target="_block">КУПИТЬ</a></li>
+        
+      </ul>
+      <script>
+$('.nav-toggle').on('click', function(){
+  $('#menu').toggleClass('active');
+   $('#header').toggleClass('active');
+   $('#burger').toggleClass('active');
+});
+
+</script>
+            <div class="p-2 bd-highlight"></div>
+     </div>
+  </div>
+</nav>
